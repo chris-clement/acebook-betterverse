00e2cb495793c21d9ce3f3bd2521b6876512340b
Chris Clement <chris.clement1995@gmail.com>
2022-02-21 15:03:02 +0000

    removal of unnecessary routing spec. Routes not using 'personal' removed

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   resources :posts do
     resources :likes
     resources :comments
-    member do
-      get 'personal'
-    end
+    # member do
+    #   get 'personal'
+    # end
   end
 
   get '/profile', to: 'profile#index'
